531155398b9a26d0f28216e64647d897a03a5d16
ry211chan <ruatannov.02sc5@gmail.com>
2025-06-14 01:16:32 +0900

    6/13

--- app/src/main/java/com/example/orcaquiz/MainActivity.java
+++ app/src/main/java/com/example/orcaquiz/MainActivity.java
@@ -8,6 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.orcaquiz.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;  //自動で追加してくれている
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -15,7 +19,14 @@ public class MainActivity extends AppCompatActivity {
     private int rightAnswerCount; //正解数をカウントする
     private int quizCount  = 1; //今何問目かカウントする
 
-    private String[][] quizData = {}
+
+    private ArrayList<ArrayList<String>> quizarray = new ArrayList<>();
+    private String[][] quizData = {
+            // {"問題","正解","選択肢１","選択肢２","選択肢３","選択肢４"} 配列の中に配列→多次元配列
+            {"シャチは何の仲間？","魚類","哺乳類","爬虫類","両生類"},
+            {"シャチは超音波で周囲を探る能力があるが、その能力を何と呼ぶか？","ソナー","エコーロケーション","サブマリンセンス","音響探査"}
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +39,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        for (int i =0; i < quizData.length; i++) {
+            ArrayList<String> quiz = new ArrayList<>(); //ArrayList　配列の要素数を決めておかなくてよい
+            quiz.add(quizData[i][0]); //問題
+            quiz.add(quizData[i][1]); //正解
+            quiz.add(quizData[i][2]); //選択肢１
+            quiz.add(quizData[i][3]); //選択肢２
+            quiz.add(quizData[i][4]); //選択肢３
+            quiz.add(quizData[i][5]); //選択肢４
+            //add　リストに要素を追加する
+
+            quiz.add()
+        }
     }
 }
